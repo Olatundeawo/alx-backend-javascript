f7147f1f2f42d13ddf51d5ecb57f7e9ee62d6378
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-06-29 02:30:57 -0500

    modifty

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,13 @@
 export default function guardrail(mathFunction) {
-  const queue = [];
+    let queue = [];
 
-  try {
-    queue.push(mathFunction());
-  } catch (err) {
-    queue.push(String(err));
-  } finally {
-    queue.push('Guardrail was processed');
-  }
+    try {
+        queue.push(mathFunction());
+    } catch (e) {
+        queue.push(String(e));
+    } finally {
+        queue.push('Guardrail was processed');
+    }
 
-  return queue;
+    return queue;
 }
