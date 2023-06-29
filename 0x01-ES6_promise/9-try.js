dbfbedc1acd2018b02bdc876002ed01ad1181a32
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-06-29 05:56:08 -0500

    added some files

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+    let queue = [];
+
+    try {
+        queue.push(mathFunction());
+    } catch (e) {
+        queue.push(String(e));
+    } finally {
+        queue.push('Guardrail was processed');
+    }
+
+    return queue;
+}
