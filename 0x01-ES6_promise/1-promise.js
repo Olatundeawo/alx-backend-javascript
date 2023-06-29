f7928ae40b792fdc189c5251f6673141e4e4ab41
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-06-29 05:49:53 -0500

    added a readme file

--- 0x01-ES6_promise/1-promise.js
+++
@@ -1,9 +0,0 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({ status: 200, body: 'success' });
-    } else {
-      reject('The fake API is not working currently');
-    }
-  });
-}
--- 0x01-ES6_promise/4-user-promise.js
+++
@@ -1,5 +0,0 @@
-export default function signUpUser(firstName, lastName) {
-    return new Promise((resolve, reject) => {
-        resolve({ firstName: `${firstName}`,  lastName: `${lastName}`})
-    });
-}
