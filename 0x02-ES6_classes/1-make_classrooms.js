dac767e206aa19a3d5beadd4a16853aa0ea61008
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-06-30 09:11:17 -0500

    modify soe files

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,6 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    let myArray = [19, 20, 34].map((res) => new ClassRoom(res));
-    return myArray;
-
+  const myArray = [19, 20, 34].map((res) => new ClassRoom(res));
+  return myArray;
 }
