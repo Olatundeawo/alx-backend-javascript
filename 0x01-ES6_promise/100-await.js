264bbb51bff9513c0e4cd7571ec3f30e341d0d56
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-06-30 00:07:38 -0500

    mod

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,16 +1,16 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    let obj = {};
-    try {
+  let obj = {};
+  try {
 
-        let photo = await uploadPhoto();
-        let user = await createUser();
-        obj = {photo, user};
-    } catch (e) {
-        obj = {photo: null, user: null};
-    }
+      let photo = await uploadPhoto();
+      let user = await createUser();
+      obj = {photo, user};
+  } catch (e) {
+      obj = {photo: null, user: null};
+  }
 
-    return obj;
+  return obj;
 
 }
